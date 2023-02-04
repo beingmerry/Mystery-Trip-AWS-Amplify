8b4be3574968d9b67de99bf07f28730ad0df4139
Ben Merryman <ben.merryman@gmail.com>
2023-02-04 14:13:38 -0500

    attempted fix for Amplify issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+// modified file per https://github.com/aws-amplify/amplify-js/issues/10242#issuecomment-1399596417
 import react from '@vitejs/plugin-react-swc'
+
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  ...(process.env.NODE_ENV === 'development'
+    ? {
+        define: {
+          global: {}
+        }
+      }
+    : {}),
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      ...(process.env.NODE_ENV !== 'development'
+        ? {
+            './runtimeConfig': './runtimeConfig.browser' //fix production build
+          }
+        : {})
+    }
+  }
 })
